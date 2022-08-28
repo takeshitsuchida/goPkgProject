70537f4ad6c91d77030a09ac1cc8a075776ae0a9
ttsuchid <ttsuchid@yahoo-corp.jp>
2022-08-28 09:35:09 +0900

    web application

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goPkgProject
+
+Writing Web Applications
+https://go.dev/doc/articles/wiki/
+
+https://www.udemy.com/course/go-fintech/learn/lecture/12070392#overview
--- main.go
+++ main.go
@@ -1,93 +1,91 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"io/ioutil"
 	"log"
-
-	_ "github.com/mattn/go-sqlite3"
+	"net/http"
+	"regexp"
+	"text/template"
 )
 
-var DbConnection *sql.DB
+type Page struct {
+	Title string
+	Body  []byte
+}
 
-type Person struct {
-	Name string
-	Age  int
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+var templates = template.Must(template.ParseFiles("view.html", "edit.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	// t, _ := template.ParseFiles(tmpl + ".html")
+	// t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	// /view/test
+	// title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	// /edit
+	// title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	// /edit
+	// title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	fmt.Println(string(p.Body))
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+var validPath = regexp.MustCompile("^/(edit|view|save)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
 }
 
 func main() {
-	DbConnection, _ := sql.Open("sqlite3", "./example.sql")
-	defer DbConnection.Close()
-	cmd := `CREATE TABLE IF NOT EXISTS person(
-						name STRING,
-						age  INT)`
-	_, err := DbConnection.Exec(cmd)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// insert/update
-	// cmd = "INSERT INTO person (name, age) VALUES (?, ?)"
-	// _, err = DbConnection.Exec(cmd, "Smith", 20)
-	// cmd = "UPDATE person SET age = ? WHERE name = ?"
-	// _, err = DbConnection.Exec(cmd, 40, "Mike")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-
-	// select
-	// cmd = "SELECT * FROM person"
-	// rows, _ := DbConnection.Query(cmd)
-	// defer rows.Close()
-	// var pp []Person
-	// for rows.Next() {
-	// 	var p Person
-	// 	rows.Scan(&p.Name, &p.Age)
-	// 	pp = append(pp, p)
-	// }
-	// err = rows.Err()
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// for _, p := range pp {
-	// 	fmt.Println(p.Name, p.Age)
-	// }
-
-	// select
-	// cmd = "SELECT * FROM person where age = ?"
-	// row := DbConnection.QueryRow(cmd, 100)
-	// var p Person
-	// err = row.Scan(&p.Name, &p.Age)
-	// if err != nil {
-	// 	if err == sql.ErrNoRows {
-	// 		log.Println("No row")
-	// 	} else {
-	// 		log.Println(err)
-	// 	}
-	// }
-	// fmt.Println(p.Name, p.Age)
-
-	// delete
-	// cmd = "DELETE FROM person WHERE name = ?"
-	// _, err = DbConnection.Exec(cmd, "Nancy")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-
-	// 値ではなくテーブル名を変数として入れたい場合
-	tableName := "person"
-	cmd = fmt.Sprintf("SELECT * FROM %s", tableName)
-	row := DbConnection.QueryRow(cmd)
-	var p Person
-	err = row.Scan(&p.Name, &p.Age)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			log.Println("No row")
-		} else {
-			log.Println(err)
-		}
-	}
-	fmt.Println(p.Name, p.Age)
-
-	fmt.Println("###end###")
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
